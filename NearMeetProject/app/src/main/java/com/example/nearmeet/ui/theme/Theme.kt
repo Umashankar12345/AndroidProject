dc87240b43053ad0aef83e3c8b1ab0f69872aa5b
Umashankar Kumar <umashankarkumar9572@gmail.com>
2026-05-07 13:41:17 +0530

    my message

--- NearMeetProject/app/src/main/java/com/example/nearmeet/ui/theme/Theme.kt
+++ NearMeetProject/app/src/main/java/com/example/nearmeet/ui/theme/Theme.kt
@@ -5,9 +5,11 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = NearMeetPrimary,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = NearMeetBackground,
+    surface = NearMeetSurface
 )
 
 @Composable

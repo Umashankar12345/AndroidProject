5e158ffbe9d427899c413ed0c4f3e3101d3c0b94
Umashankar Kumar <umashankarkumar9572@gmail.com>
2026-05-14 00:27:50 +0530

    first commit

--- NearMeetProject/app/src/main/java/com/example/nearmeet/data/model/User.kt
+++ NearMeetProject/app/src/main/java/com/example/nearmeet/data/model/User.kt
@@ -4,6 +4,7 @@ data class User(
     val uid: String = "",
     val name: String = "",
     val email: String = "",
+    val profileImageUrl: String? = null,
     val fcmToken: String = "",
     val joinedEvents: List<String> = emptyList()
 )
